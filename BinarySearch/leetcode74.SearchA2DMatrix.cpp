1a52db6f8e8033ee9173f03222e007da9f33830b
svdalv0313 <suqiang0313@gmail.com>
2017-03-19 20:37:05 +0800

    suqiang

--- BinarySearch/leetcode74.SearchA2DMatrix.cpp
+++ BinarySearch/leetcode74.SearchA2DMatrix.cpp
@@ -64,3 +64,36 @@ public:
         }
     }
 };
+// another
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        if(matrix.size()==0||matrix[0].size()==0)
+        {
+            return false;
+        }
+        int M=matrix.size();
+        int N=matrix[0].size();
+        int i=0;
+        int j=N-1;
+        while(i<M&&j>=0)
+        {
+            if(matrix[i][j]==target)
+            {
+                return true;
+            }
+            if(matrix[i][j]>target)
+            {
+                j--;
+
+            }
+            if(matrix[i][j]<target)
+            {
+                i++;
+            }
+        }
+        return false;
+        
+    }
+};
